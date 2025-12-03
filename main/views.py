4bfd4f7557fa61f59ea4e9f21df86fceea01fda3
Prateek Gupta <prateekjpg@gmail.com>
2025-12-03 13:34:10 +0530

    add register to urls and views

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from main.forms import SignUpForm
+
+
 # Create your views here.
 def home(request):
     # check to see if logging in
@@ -27,3 +30,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You are now logged out')
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+
+            messages.success(request, 'You are now registered')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'register.html', {'form': form})
